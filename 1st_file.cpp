9c3a9f8cb383628f6a2d93eb4c4247f3982b9d45
JainamKhetani <24cs039@charusat.edu.in>
2024-12-26 14:08:56 +0530

    First Commit

---
+++ 1st_file.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>  // For setprecision, fixed, and scientific
+using namespace std;
+
+int main(){
+    double e = 14049.304930000;  // Example with trailing zeros in memory
+    
+    cout << fixed << setprecision(5) << e << endl;  // This prints the number in fixed-point notation with 5 decimal places
+    
+    return 0;
+}
