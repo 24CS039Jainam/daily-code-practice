e0a8fa16ee54c9ffdb24b08760cbcb452e4fb60a
JainamKhetani <24cs039@charusat.edu.in>
2024-12-26 14:10:37 +0530

    Initial commit

--- 1st_file.cpp
+++ 1st_file.cpp
@@ -6,6 +6,6 @@ int main(){
     double e = 14049.304930000;  // Example with trailing zeros in memory
     
     cout << fixed << setprecision(5) << e << endl;  // This prints the number in fixed-point notation with 5 decimal places
-    
+    cout<<"Thank You...."<<endl;
     return 0;
 }
