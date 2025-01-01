b254d6845c859fbb97be5c7efd801947f859b1f7
JainamKhetani <24cs039@charusat.edu.in>
2025-01-01 07:46:59 +0530

    Add L.C.M.

---
+++ PLATFORMS/Codeforces/Normal problem.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    char arr[n];
+
+    for ( int i = 0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+int i,j;
+    for( i = 0 , j=n ; i<j; i++, j--)
+       {
+        arr[i]=
+       }
+    return 0;
+}
