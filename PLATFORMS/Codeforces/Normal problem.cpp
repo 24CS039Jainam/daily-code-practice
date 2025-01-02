c96771d0501c484e796cd731bae9c6fc4d51a632
Khetani Jainam Anilbhai <24CS039@charusat.edu.in>
2025-01-02 21:36:44 +0530

    Update Normal problem.cpp

--- PLATFORMS/Codeforces/Normal problem.cpp
+++ PLATFORMS/Codeforces/Normal problem.cpp
@@ -39,22 +39,37 @@ qpwwpqwpqpw
 pqpqpqpq
 
 */
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main(){
+int main() {
     int n;
-    cin>>n;
-    char arr[n];
+    cin >> n;
 
-    for ( int i = 0; i<n; i++)
-    {
-        cin>>arr[i];
+    string arr[n]; 
+   
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-int i,j;
-    for( i = 0 , j=n ; i<j; i++, j--)
-       {
-        arr[i]=
-       }
+
+    
+    for (int i = 0; i < n; i++) {
+        string reversed = "";
+        for (int j = arr[i].length() - 1; j >= 0; j--) {
+            char ch = arr[i][j];
+           
+            if (ch == 'p') {
+                reversed += 'q';
+            } else if (ch == 'q') {
+                reversed += 'p';
+            } else {
+                reversed += ch;
+            }
+        }
+        cout << reversed << endl; 
+    }
+
     return 0;
 }
