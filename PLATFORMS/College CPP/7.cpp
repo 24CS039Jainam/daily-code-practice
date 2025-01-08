7800aced5caaabb8c709025630728e40319c0ffc
JainamKhetani <24cs039@charusat.edu.in>
2025-01-08 12:45:13 +0530

    Updated Code

--- PLATFORMS/College CPP/7.cpp
+++ PLATFORMS/College CPP/7.cpp
@@ -1,22 +1,31 @@
-#include<iostream>
+/*A. Theatre Square
+time limit per test1 second
+memory limit per test256 megabytes
+Theatre Square in the capital city of Berland has a rectangular shape with the size n × m meters. On the occasion of the city's anniversary, a decision was taken to pave the Square with square granite flagstones. Each flagstone is of the size a × a.
+
+What is the least number of flagstones needed to pave the Square? It's allowed to cover the surface larger than the Theatre Square, but the Square has to be covered. It's not allowed to break the flagstones. The sides of flagstones should be parallel to the sides of the Square.
+
+Input
+The input contains three positive integer numbers in the first line: n,  m and a (1 ≤  n, m, a ≤ 109).
+
+Output
+Write the needed number of flagstones.
+
+Examples
+InputCopy
+6 6 4
+OutputCopy
+4
+*/
+#include <iostream>
 using namespace std;
 
-int main(){
-    int n,m,a;
-    cout<<"Enter n :- ";
-    cin>>n;
-    cout<<"Enter m :- ";
-    cin>>m;
-    cout<<"Enter a :- ";
-    cin>>a;
-    int larger_area=n*m;
-    int smaller_area=a*a;
-    for(int i=1 ; i<=n*m ; i++){
-       
-        if(i*smaller_area>=larger_area){
-            cout<<i<<endl;
-            break;
-        }
-    }
-       return 0;
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+    long long flagstones_length = (n + a - 1) / a;
+    long long flagstones_width = (m + a - 1) / a;
+    long long total_flagstones = flagstones_length * flagstones_width;
+    cout << total_flagstones << endl;
+    return 0;
 }
